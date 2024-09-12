85b5a524cc1ada787ad9bdc29457606f1b24894e
Joanna Musialik <joanna.m.musialik@gmail.com>
2024-09-12 08:55:49 +0200

    Add comments to ILogHandler

--- iloghandler.h
+++ iloghandler.h
@@ -3,12 +3,28 @@
 
 #include <vector>
 
+///
+/// \brief Interface allowing to handle logs containing points achieved during one game.
+///
+/// ILogHandler is used to save and read game points. It's an abstraction made to separate implementation.
 class ILogHandler
 {
 public:
+    ///
+    /// Basic ILogHandler constructor.
     ILogHandler();
-    virtual void SaveGame(int) = 0;
+
+    ///
+    /// Pure virtual member to save game points.
+    /// @param _points  The points achieved during game.
+    virtual void SaveGame(int _points) = 0;
+
+    ///
+    /// Pure virtual member to read game points saved.
     virtual std::vector<int> ReadGames() = 0;
+
+    ///
+    /// Virtual destructor ~ILogHandler.
     virtual ~ILogHandler();
 };
 
