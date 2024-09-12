c3fef9ae82557beb40b12066d9128bed87336dea
Joanna Musialik <joanna.m.musialik@gmail.com>
2024-09-12 09:45:39 +0200

    Add comments in GameMode enum

--- enums/gamemode.h
+++ enums/gamemode.h
@@ -1,6 +1,10 @@
 #ifndef GAMEMODE_H
 #define GAMEMODE_H
 
+///
+/// \brief The GameMode enum.
+///
+/// The GameMode enum represents the game mode in the UI classes.
 enum class GameMode {
     EASY = 0,
     MEDIUM = 1,
