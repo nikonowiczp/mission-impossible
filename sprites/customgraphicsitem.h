2b117ac6e8ad69f2d6eec209f66e2dd46f822bd9
Joanna Musialik <joanna.m.musialik@gmail.com>
2024-09-12 09:43:47 +0200

    Add comments to CustomGraphicsItem class

--- sprites/customgraphicsitem.h
+++ sprites/customgraphicsitem.h
@@ -4,11 +4,28 @@
 #include <QGraphicsRectItem>
 #include <QGraphicsPixmapItem>
 
+///
+/// \brief The CustomGraphicsItem class representing objects on the game map.
+///
+/// The CustomGraphicsItem class derives from QGraphicsPixmapItem. Its main responsibility is to render the object with the given asset on a given position.
 class CustomGraphicsItem : public QGraphicsPixmapItem
 {
 public:
-    CustomGraphicsItem(std::string, int, int, int, int, QGraphicsItem* _parent = nullptr);
-    void Move(int, int);
+
+    ///
+    /// CustomGraphicsItem constructor.
+    /// @param _assetPath   Path to the asset representing the object.
+    /// @param _assetId     Id of the object as int.
+    /// @param _x           Initial x coordinate.
+    /// @param _y           Initial y coordinate.
+    /// @param _parent      Pointer to the QGraphicsItem parent of the object.
+    CustomGraphicsItem(std::string _assetPath, int _assetId, int _x, int _y, int _radius, QGraphicsItem* _parent);
+
+    ///
+    /// Public method used to move the object to the given place on map.
+    /// @param _x   X coordinate of the target position.
+    /// @param _y   Y coordinate of the target position.
+    void Move(int _x, int _y);
 private:
     int assetId;
     std::string assetPath;
