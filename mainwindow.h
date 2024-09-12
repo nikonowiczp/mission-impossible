74f09de1dc24064513e80d6bf8a774615036de8c
Joanna Musialik <joanna.m.musialik@gmail.com>
2024-09-12 09:24:07 +0200

    Add comments to MainWindow class

--- mainwindow.h
+++ mainwindow.h
@@ -13,15 +13,34 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+///
+/// \brief The MainWindow class derived from QMainWindow class, handling the main window.
+///
+/// The MainWindow class is used to generate all the UI in the app using QT 6 framework.
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(std::unique_ptr<GameManager>, std::unique_ptr<ILogHandler>, QWidget *_parent = nullptr);
+
+    ///
+    /// MainWindow constructor.
+    /// @param _manager     Unique pointer to GameManager class, used to manage the logic.
+    /// @param _logHandler  Unique pointer to ILogHandler class, used to generate and read logs.
+    /// @param _parent      Pointer to the QWidget class, parent of the current widget.
+    MainWindow(std::unique_ptr<GameManager> _manager, std::unique_ptr<ILogHandler> _logHandler, QWidget *_parent = nullptr);
+
+    ///
+    /// Basic MainWindow destructor.
     ~MainWindow();
 public slots:
+
+    ///
+    /// Public slot used to handle the ticks when the game is on.
     void DoTick();
+
+    ///
+    /// Public slot used to handle esc key click during the game.
     void OnEscDuringGame();
 private slots:
 
